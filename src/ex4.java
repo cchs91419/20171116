e3b6985fe3f120cadc098ca9d331cc0805d23c71
cchs91419 <32055781+cchs91419@users.noreply.github.com>
2017-11-06 15:54:58 +0800

    Add files via upload

---
+++ src/ex4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex4 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+        int sum = 0;
+        for (int i = x ; x<=y ; x++)
+        { sum = sum + x; }
+        System.out.println(sum);
+    }
+}
+
+
