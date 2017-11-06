a7c5e2bff9e400e8d62489b259d27428d4d96c91
cchs91419 <32055781+cchs91419@users.noreply.github.com>
2017-11-06 15:49:37 +0800

    Add files via upload

---
+++ src/ex3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex3 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        for (int i = 1; i <= x; i++) {
+            if (i % 3 == 0) {
+                System.out.print("A");
+            } else {
+                System.out.print("*");
+            }
+        }
+    }
+}
